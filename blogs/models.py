675d3119b2860cce4b483c31890d252e9d511735
Jayesh C.J <jayeshcj2003@gmail.com>
2024-09-05 10:48:18 +0530

    Models created

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Blogs Category model 
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.category_name
+    
+# Blogs model 
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='files/blogs')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
